c14abeb1e04ff3c6d8636593cad28ff7bc5228b7
nikhil-dex <jincossec@gmail.com>
2026-07-23 21:45:02 +0530

    feat: enquiry post function made

--- app/api/enquiry/route.js
+++ app/api/enquiry/route.js
@@ -1,8 +1,28 @@
-export async function POST(req) {
-    const body = await req.json();
+import connectDB from "@/lib/db";
+import Form from "@/models/Form";
 
-    return Response.json({
-        success: true,
-        body,
-    });
+export async function POST(req) {
+    try {
+        await connectDB();
+
+        const body = await req.json();
+
+        const enquiry =
+            await Form.create(body);
+
+        return Response.json({
+            success: true,
+            enquiry,
+        });
+    } catch (error) {
+        return Response.json(
+            {
+                success: false,
+                error: error.message,
+            },
+            {
+                status: 500,
+            }
+        );
+    }
 }
